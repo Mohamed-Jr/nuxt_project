4fffe0e94100fdbdfa65be4027e293227e348774
Mohame-Jr <m.bouguenaya@gmail.com>
2020-12-24 17:29:25 +0100

    premier commit

---
+++ components/Test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <p>ceci est le composant Test</p>
+      </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/Page2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 align="center">Voici la page2</h1>
+        <router-link to="/">cliquez ici</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
